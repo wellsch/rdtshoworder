8809d34b06592342269490df1f91ddf7c385216a
wellsch <games@smcewells.com>
2025-10-05 09:51:30 -0500

    remove 2024 specificities

--- textbased_greedy.py
+++ textbased_greedy.py
@@ -13,43 +13,24 @@ def weight_dances(dances: set['Dance']):
         dance.calc_weight_greedy()
 
 
-file = open("dances.txt")
+file = open("2025dances.txt")
 
 dances = set()
 all_dancers = {}
-
-magic_mike = None
-olivia = None
-grace = None
 
 for line in file:
     [name, dancers_str] = line.split(":")
     dancers_names = set(dancers_str.strip().split(","))
     dancers = set()
     for dancer in dancers_names:
-        if dancer in all_dancers:
-            all_dancers[dancer].add_dance()
+        dancer_str = dancer.strip()
+        if dancer_str in all_dancers:
+            all_dancers[dancer_str].add_dance()
         else:
-            all_dancers[dancer] = Dancer(dancer)
-        dancers.add(all_dancers[dancer])
+            all_dancers[dancer_str] = Dancer(dancer_str)
+        dancers.add(all_dancers[dancer_str])
     dance = Dance(name, dancers)
     dances.add(dance)
-    if (name == "Sol Jazz"):
-        dance.schedule_dance()
-        dances.remove(dance)
-        for dancer in dancers:
-            dancer.time_since_last_dance = 0
-    if (name == "Magic Mike"):
-        magic_mike = dance
-    if (name == "Olivia Contemp"):
-        olivia = dance
-    if (name == "Grace Musical Theatre"):
-        grace = dance
-
-dances.remove(magic_mike)
-dances.remove(olivia)
-dances.remove(grace)
-
 
 add_edges(dances)
 weight_dances(dances)
@@ -59,16 +40,6 @@ qcs = 0
 instants = 0
 
 while len(dances) != 0:
-    if num_dance == 13:
-        magic_mike.schedule_dance()
-        for dancer in all_dancers.values():
-            dancer.time_since_last_dance = float('inf')
-        olivia.schedule_dance()
-        num_dance += 2
-        weight_dances(dances)
-        for dancer in all_dancers.values():
-            dancer.time_since_last_dance += 1
-        continue
     min_dance = min(dances)
     if min_dance.weight == float('inf'):
         print("All remaining dances share a member with this dance")
@@ -83,15 +54,6 @@ while len(dances) != 0:
     for dancer in all_dancers.values():
         dancer.time_since_last_dance += 1
     num_dance += 1
-
-if grace.weight == float('inf'):
-    new_qcs, new_instants = min_dance.qcs()
-    qcs += new_qcs
-    instants += 1
-else:
-    qcs += grace.weight
-grace.schedule_dance()
-
 
 print("Quick Changes: " + str(qcs))
 print("Instants: " + str(instants))
